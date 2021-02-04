ecea186f945fd4a3119fdc71d60a32d9dc8baff8
AnnaChupis <rf2@ukr.net>
2021-02-04 13:36:01 +0200

    final functions editing and rename

--- todomvc_poc_test/model/todomvc.py
+++ todomvc_poc_test/model/todomvc.py
@@ -3,8 +3,6 @@ from selene.support.shared import browser
 
 todo_list = browser.all('#todo-list li')
 
-def edit_field(text):
-    return todo_list.element_by(have.css_class('editing')).element('.edit').type(text)
 
 def open():
     browser.open('https://todomvc4tasj.herokuapp.com').\
@@ -15,24 +13,33 @@ def add(*todos: str):
     for todo in todos:
         browser.element('#new-todo').type(todo).press_enter()
 
-def edit(todo: str,new_text):
+
+def rewrite_selected(todo, new_text):
     todo_list.element_by(have.exact_text(todo)).double_click()
-    edit_field(new_text).perform(command.js.set_value(new_text)).press_enter()
+    return todo_list.element_by(have.css_class('editing')).element('.edit').\
+        perform(command.js.set_value(new_text))
+
+
+def edit(todo: str,new_text):
+    rewrite_selected(todo,new_text).press_enter()
+
 
 def toggle(todo: str):
     todo_list.element_by(have.exact_text(todo)).element('.toggle').click()
 
+
 def clear_completed():
     browser.element('#clear-completed').click()
 
+
 def cancel_edit(todo: str,new_text):
-   todo_list.element_by(have.exact_text(todo)).double_click()
-   edit_field(new_text).press_escape()
+   rewrite_selected(todo,new_text).press_escape()
+
 
 def delete(todo: str):
     todo_list.element_by(have.exact_text(todo)).hover().element('.destroy').click()
 
+
 def should_have(*todos: str):
         todo_list.should(have.exact_texts(*todos))
-
 
